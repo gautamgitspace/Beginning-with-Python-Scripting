c1294d71f15659e108564efd1e753bb7f2aff796
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-14 21:02:13 -0400

    added try and except handlers

--- playingWithNumbers.py
+++ playingWithNumbers.py
@@ -1,9 +1,13 @@
 # covers user input, lists, loops and finds count, sum, average, search, detect, smallest, largest in a set of numbers
 array_of_numbers = list()
 number =  raw_input('Enter the number of elements you want in the array: ')
-for i in range (int(number)):
-	n = raw_input('number: ')
-	array_of_numbers.append(int(n))
+try:
+    for i in range (int(number)):
+        n = raw_input('number: ')
+        array_of_numbers.append(int(n))
+except:
+    print 'INVALID INPUT. MUST BE A NUMERIC'
+    exit()
 print 'Here is your array: ', array_of_numbers
 count=0
 sum=0
@@ -24,21 +28,28 @@ print 'Average is: ', sum/count
 print 'Largest element is: ', largest
 print 'Smallest element is: ', smallest
 search_critera = raw_input('Search numbers greater than ? ')
-n = int(search_critera)
-none_found=True
-for i in array_of_numbers:
-    if i>n:
-        print i, 'is greater than', n
-        none_found=False
-if none_found==True:
+try:
+    n = int(search_critera)
+    none_found=True
+    for i in array_of_numbers:
+        if i>n:
+            print i, 'is greater than', n
+            none_found=False
+    if none_found==True:
         print 'none found'
+except:
+    print 'INVALID INPUT. MUST BE A NUMERIC'
+    exit()
 found = False
 search_critera_2 = raw_input('Check if a number exists in the array. Tell me what to find? ')
-n = int(search_critera_2)
-for i in array_of_numbers:
-    if i==n:
-        print 'Found', i
-        found=True
-        break
-if found==False:
+try:
+    n = int(search_critera_2)
+    for i in array_of_numbers:
+        if i==n:
+            print 'Found', i
+            found=True
+            break
+    if found==False:
         print 'Not found'
+except:
+    print 'INVALID INPUT. MUST BE A NUMERIC'
