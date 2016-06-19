2e5087fbfc1d7f383ccceb830ef86d46873c3fa8
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-19 13:10:49 -0400

    hackerrank intro-swaptwousingtuples

---
+++ swaptwonumbers.py
@@ -0,0 +1,10 @@
+#swap numbers using tuples
+one = raw_input("enter first number ")
+two = raw_input("enter second number ")
+o=int(one)
+t=int(two)
+x,y=t,o
+print 'first number is: ', x
+print 'second number is: ', y
+
+
