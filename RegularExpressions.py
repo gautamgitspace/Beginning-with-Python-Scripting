153c9ca27bc869ca731f866b59a96550aec21d01
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-23 15:36:16 -0400

    more regex

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -4,12 +4,38 @@ fineHandle = open('mbox.txt')
 count=0
 for line in fineHandle:
     line=line.rstrip()
-    if re.search('^X.*', line):
+    if re.search('^X.+:', line):
         count=count+1
         print line
 print 'COUNT: ', count
 
 #findall gives back a python list
 x = 'My shoe size is 9 (US) which is 8 (UK). Sometimes 10 (US) also fits.'
+f = 'From: agautam2@buffalo.edu To: ubwins.cse@buffalo.edu'
 y = re.findall('[0-9]+', x)
-print 'Here you go, all the numbers returned in the form of a python list: ', y
+g = re.findall('[AEIOU]', x)
+r = re.findall('^F.+:',f)
+b = re.findall('^F.+?:',f)
+print 'Here you go! all the numbers returned in the form of a python list: ', y
+print 'Here you go! all the caps vowels returned in the form of a python list: ', g
+print 'Greedy approach(longest match): ', r
+print 'Non Greedy approach(short match): ', b
+
+# finding everything to the left and right of '@'
+
+fileHandle2 = open ('mbox.txt')
+for line in fileHandle2:
+    line=line.rstrip()
+    if re.search('^From.*', line):
+        s=line
+        g=re.findall('\S+@\S+', s)
+        data=g[0]
+        atpos=data.find('@')
+        spacepos=data.find(' ', atpos)
+        #using string slicing now to get the hostname
+        hostname = data[atpos+1: spacepos]
+        print hostname
+
+
+
+
