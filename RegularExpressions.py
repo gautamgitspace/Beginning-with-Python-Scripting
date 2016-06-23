a4e087f94fe131c3f68112d6861ce37f75a6770b
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-23 16:17:22 -0400

    more cool regex

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -22,19 +22,42 @@ print 'Greedy approach(longest match): ', r
 print 'Non Greedy approach(short match): ', b
 
 # finding everything to the left and right of '@'
+# can also be done by double split
 
 fileHandle2 = open ('mbox.txt')
 for line in fileHandle2:
     line=line.rstrip()
     if re.search('^From.*', line):
-        s=line
-        g=re.findall('\S+@\S+', s)
-        data=g[0]
-        atpos=data.find('@')
-        spacepos=data.find(' ', atpos)
+        s = line
+        g = re.findall('\S+@\S+', s)
+        data = g[0]
+        atpos = data.find('@')
+        spacepos = data.find(' ', atpos)
         #using string slicing now to get the hostname
         hostname = data[atpos+1: spacepos]
-        print hostname
+        print 'using regex and slicing:', hostname
+
+# doing the same using the NOT operator
+fileHandle = open('mbox.txt')
+for line in fileHandle:
+    line = line.rstrip()
+    if re.findall('^From .*@([^ ]*)', line):
+        y = re.findall('^From .*@([^ ]*)', line)
+        print 'using ^ operator:', y
+
+# printing the largest float in the lines resembling: 'X-DSPAM-Confidence: some float value"
+listOfIntegers = list()
+fileHandle3 = open('mbox.txt')
+for line in fileHandle3:
+    line = line.rstrip()
+    if re.findall('^X-DSPAM-Confidence:.*', line):
+        h = re.findall('^X-DSPAM-Confidence:.*', line)
+        #print h[0]
+        splitter = h[0].split(':')
+        a = float(splitter[1])
+        listOfIntegers.append(a)
+print '\nmax of all float values: ', max(listOfIntegers)
+
 
 
 
