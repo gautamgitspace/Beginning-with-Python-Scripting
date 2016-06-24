4d5c072963204d31fceb5fa368b8252eb3c32cd5
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-24 16:06:49 -0400

    parsing XML

--- network.py
+++ network.py
@@ -2,33 +2,33 @@
 #run assignment1 in server mode on port 3369 first
 
 import socket
-# mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# mysocket.connect(('www.py4inf.com', 80))
-#
-# mysocket.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.1\n\n')
-#
-# while True:
-#     data = mysocket.recv(512)
-#     if len(data) < 1:
-#         print 'nothing'
-#         break
-#     print data
-# mysocket.close()
 
-import urllib
-d=dict()
-m=list()
-bigWord = None
-bigCount = None
-pageHandle = urllib.urlopen('http://www.py4inf.com/code/words.txt')
-for line in pageHandle:
-    line=line.strip()
-    words=line.split()
-    for word in words:
-        d[word]=d.get(word,0)+1
-        for key, value in d.items():
-            #print key, value
-            if bigWord is None or value > bigCount:
-                bigCount=value
-                bigWord=key
-print bigWord, bigCount
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.py4inf.com', 80))
+mysock.send('GET http://www.pythonlearn.com/code/intro-short.txt HTTP/1.0\n\n')
+
+while True:
+    data = mysock.recv(512)
+    if ( len(data) < 1 ) :
+        break
+    print data;
+
+mysock.close()
+
+# import urllib
+# d=dict()
+# m=list()
+# bigWord = None
+# bigCount = None
+# pageHandle = urllib.urlopen('http://www.py4inf.com/code/words.txt')
+# for line in pageHandle:
+#     line=line.strip()
+#     words=line.split()
+#     for word in words:
+#         d[word]=d.get(word,0)+1
+#         for key, value in d.items():
+#             #print key, value
+#             if bigWord is None or value > bigCount:
+#                 bigCount=value
+#                 bigWord=key
+# print bigWord, bigCount
---
+++ parsingxml.py
@@ -0,0 +1,60 @@
+#parsing XML data in python (DE-SERIALIZE)
+import xml.etree.ElementTree as ET
+
+data = '''
+<person>
+  <name>Vin</name>
+  <phone type="intl">
+     +1 716 431 9771
+   </phone>
+   <email hide="yes"/>
+</person>'''
+
+print 'parsing 1st snippet\n'
+tree = ET.fromstring(data)
+print 'Name:', tree.find('name').text
+print 'Phone:', tree.find('phone').text
+print 'Attr:', tree.find('email').get('hide')
+
+input = '''
+<stuff>
+    <users>
+        <user x="2">
+            <team>Philadelphia</team>
+            <name>Allen</name>
+            <position>Point Guard</position>
+        </user>
+        <user x="3">
+            <team>Cleveland</team>
+            <name>Kyrie</name>
+            <position>Point Guard</position>
+            </user>
+        <user x="4">
+            <team>Boston</team>
+            <name>Kevin</name>
+            <position>Small Forward</position>
+        </user>
+        <user x="5">
+            <team>Boston</team>
+            <name>Paul</name>
+            <position>Power Forward</position>
+        </user>
+        <user x="6">
+            <team>New York</team>
+            <name>Carmelo</name>
+            <position>Power Forward</position>
+        </user>
+        </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+#findall returns a list
+l = stuff.findall('users/user')
+print 'parsing 2nd snippet\n'
+print 'User count:', len(l)
+
+for item in l:
+    print 'Name', item.find('name').text
+    print 'Position', item.find('position').text
+    print 'Team', item.find('team').text
+
