c9f11a7c1d330ca4204150879b5bf997a929725f
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-25 02:18:04 -0400

    write output to file in append mode

--- coordinateResolver.py
+++ coordinateResolver.py
@@ -2,31 +2,38 @@ import urllib
 import json
 
 serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
-
+print "I'll try to geo-resolve the location entered by you. type 'DONE' when you're done"
 while True:
-    address = raw_input('\nEnter the location: ')
-    if len(address) < 1:
-        print 'Please verify your address'
-        break
-    url = serviceurl + urllib.urlencode({'sensor': 'false', 'address': address})
-    print 'NEW URL:', url
-    urlHandle = urllib.urlopen(url)
-    data = urlHandle.read()
-    length = len(data)
-    print 'RECEIVED BYTES OF DATA:', length
     try:
-        jsdata = json.loads(data)
+        address = raw_input('\nEnter the location: ')
+        if address=="done":
+            break
+        url = serviceurl + urllib.urlencode({'sensor': 'false', 'address': address})
+        print 'NEW URL:', url
+        urlHandle = urllib.urlopen(url)
+        data = urlHandle.read()
+        length = len(data)
+        print 'RECEIVED BYTES OF DATA:', length
+        try:
+            jsdata = json.loads(data)
+        except:
+            jsdata = None
+        if 'status' not in jsdata or jsdata['status']!='OK':
+            print 'could not retrieve'
+        #print json.dumps(jsdata, indent=4)
+        latitude = jsdata["results"][0]["geometry"]["location"]["lat"]
+        longitude = jsdata["results"][0]["geometry"]["location"]["lng"]
+        print 'LATITUDE:', latitude
+        print 'LONGITUDE:', longitude
+        resolvedAddress =  jsdata["results"][0]["formatted_address"]
+        print resolvedAddress
+        fileHandle = open('output.txt', 'a')
+        outputString = str(latitude)+ " " + str(longitude)+ " " + str(resolvedAddress) + "\n"
+        fileHandle.write(outputString)
     except:
-        jsdata = None
-    if 'status' not in jsdata or jsdata['status']!='OK':
-        print 'could not retrieve'
-    #print json.dumps(jsdata, indent=4)
-    latitude = jsdata["results"][0]["geometry"]["location"]["lat"]
-    longitude = jsdata["results"][0]["geometry"]["location"]["lng"]
-    print 'LATITUDE:', latitude
-    print 'LONGITUDE:', longitude
-    resolvedAddress =  jsdata["results"][0]["formatted_address"]
-    print resolvedAddress
+        print 'INVALID INPUT FOOL'
+
+
 
 
 
